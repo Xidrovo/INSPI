9b9d6ab0b3b598c253d06a190ef83fb72a79e3e8
Ivan Mera <imera92@gmail.com>
2018-10-11 23:29:54 -0500

    Correción en método para creación de plantillas

--- api/inspi/api/views.py
+++ api/inspi/api/views.py
@@ -3,49 +3,49 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from api.models import Plantilla, Seccion, TipoDeDato, Pregunta
 import ast
+import json
 
-# Create your views here.
 @csrf_exempt
 def crearPlantilla(request):
 	if request.method == "POST":
-		#EXTRAER REPRESENTACION DE JSON EN STRING
-		plantilla = request.POST.get("PLANTILLA", None)
+		# EXTRAER REPRESENTACION DE JSON EN STRING
+		plantilla = request.body.decode("utf-8")
 
-		#TRANSFORMAR STRING A DICCIONARIO
-		plantillaJSON = ast.literal_eval(plantilla)
+		# TRANSFORMAR STRING A DICCIONARIO
+		plantillaJSON = json.loads(plantilla)
 
-		#			***	EXTRACCION DE DATOS ***
+		plantillaJSON = plantillaJSON["PLANTILLA"]
+
+		# EXTRACCION DE DATOS
 		titulo_plantilla = plantillaJSON["TITULO_PLANTILLA"]
 		secciones = plantillaJSON["SECCIONES"]
 
-		#CREACION DE OBJETO PLANTILLA
+		# CREACION DE OBJETO PLANTILLA
 		plantilla_obj = Plantilla().crear(titulo_plantilla)
-
 
 		for seccion in secciones:
 			titulo_seccion = seccion["TITULO_SECCION"] 
 			preguntas = seccion["PREGUNTAS"]
 
-			#CREACION DE OBJETO SECCION
+			# CREACION DE OBJETO SECCION
 			seccion_obj = Seccion().crear(titulo_seccion, plantilla_obj)
-
 
 			for pregunta in preguntas:
 				titulo_pregunta = pregunta["TITULO_PREGUNTA"]
 				descripcion = pregunta["DESCRIPCION"]
 				requerido = pregunta["REQUERIDO"] in ["True","true"]
 				detalle = pregunta["DETALLE"]
-				tipo_de_dato = pregunta["TIPO_DATO"]
+				tipo_de_dato_id = pregunta["TIPO_DATO"]
 
-				#BUSCAR EL TIPO DE DATO
+				# BUSCAR EL TIPO DE DATO
 				try:
-					tipo_de_dato = TipoDeDato.objects.filter(nombre=tipo_de_dato).first()
-				except: #SI NO ENCUENTRA EL TIPO DE DATO SE RETORNA UN TIPO DE DATOS 'DESCONOCIDO'
-					tipo_de_dato = TipoDeDato().objects.filter(nombre="DESCONOCIDO").first()
+					tipo_de_dato = TipoDeDato.objects.get(pk=tipo_de_dato_id)
+				# SI NO ENCUENTRA EL TIPO DE DATO SE RETORNA UN TIPO DE DATOS 'DESCONOCIDO'
+				except:
+					tipo_de_dato = TipoDeDato.objects.get(nombre="desconocido")
 
-				#CREACION DE OBJETO PREGUNTA
+				# CREACION DE OBJETO PREGUNTA
 				pregunta_obj = Pregunta().crear(titulo_pregunta, descripcion, requerido, detalle, seccion_obj, tipo_de_dato)
-
 
 		return JsonResponse({"RESPUESTA": "EXITO"})
 	return JsonResponse({'RESPUESTA': 'ERROR_SOLICITUD'})
