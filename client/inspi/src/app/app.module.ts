b4d895e341295c61bc79c283eae3a65573a6beeb
unknown <jjroman@espol.edu.ec>
2018-10-18 22:02:18 -0500

    estructura ngrx

--- client/inspi/src/app/app.module.ts
+++ client/inspi/src/app/app.module.ts
@@ -28,6 +28,10 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
+import { AppEffects } from './store/effects/app.effects';
 
 @NgModule({
   declarations: [
@@ -54,7 +58,8 @@ import { AppEffects } from './app.effects';
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([AppEffects])
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
