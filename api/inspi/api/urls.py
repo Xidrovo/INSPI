47fd71bd37a9818256f8ca2404a1b61da8cf18d5
Paul Estrada <PaulEstrada@MacBook-Pro-de-Paul-2.local>
2018-10-23 22:45:13 -0500

    Fix in urls.py

--- api/inspi/api/urls.py
+++ api/inspi/api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 from api import views
-from api.views import PlantillaView
+from api.views import PlantillaView, ProgramaView
 
 urlpatterns = [
    re_path(r'^plantillas/(?P<plantilla_id>[0-9]*|)$', PlantillaView.as_view(), name='plantillas'),
