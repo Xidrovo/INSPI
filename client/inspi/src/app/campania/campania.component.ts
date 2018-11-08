e0b78a2fe1354e9c728b9f307fbf224ed902a151
DarNtn <dncarpio@fiec.espol.edu.ec>
2018-11-08 13:39:47 -0500

    Fix campania component

--- client/inspi/src/app/campania/campania.component.ts
+++ client/inspi/src/app/campania/campania.component.ts
@@ -3,10 +3,6 @@ import { ApiService } from '../api.service';
 import { Campania } from './campania.model';
 import { Router } from '@angular/router';
 import { Plantilla } from '../plantillas/plantilla.model'
-import { Pregunta } from '../plantillas/pregunta.model';
-import { Seccion } from '../plantillas/seccion.model';
-
-import { PlantillasHomeComponent } from '../plantillas/plantillas-home/plantillas-home.component';
 
 @Component({
   selector: 'app-campania',
@@ -35,8 +31,7 @@ export class CampaniaComponent implements OnInit {
 
   ngOnInit() {
     this.obtenerPlantillas();
-    this.obtenerCampanias(); 
-    
+    this.obtenerCampanias();     
   }
 
   obtenerCampanias(){
@@ -54,14 +49,10 @@ export class CampaniaComponent implements OnInit {
   }
 
   private parseCampania(data: object): Campania{
-    return this.generateSchemaForm2({nombre:data['nombre'],id:data['id'],plantilla_id:data['plantilla_id'],
+    return {nombre:data['nombre'],id:data['id'],plantilla_id:data['plantilla_id'],
     fecha_inicio:data['fecha_inicio'],fecha_fin:data['fecha_fin'],
     fecha_envio_paquete:data['fecha_envio_paquete'],
-    fecha_envio_resultados:data['fecha_envio_resultados']});
-  }
-
-  private generateSchemaForm2(campania: Campania): Campania {
-    return campania;
+    fecha_envio_resultados:data['fecha_envio_resultados']};
   }
 
   async guardarCampania() {
@@ -99,94 +90,10 @@ export class CampaniaComponent implements OnInit {
       );
   }
 
-  private parsePlantilla(data: object): Plantilla{          
-      // Parsear secciones...      
-    var secciones = data['secciones'].map((s): Seccion=>{return this.parseSeccion(s)});
-                    
-    return this.generateSchemaForm({id:data['id'], titulo: data['titulo'], descripcion: data['descripcion'], secciones: secciones});
+  private parsePlantilla(data: object): Plantilla{                     
+    return {id:data['id'], titulo: data['titulo'], descripcion: data['descripcion'], secciones: data['secciones']};
   }
 
-  private parseSeccion(data: object): Seccion {
-    // Parsear preguntas...
-    var preguntas = data['preguntas'].map((pr): Pregunta=>{return this.parsePregunta(pr)});
-
-    return {id: data['id'], titulo: data['titulo'], preguntas_seccion: preguntas};
-  }
-
-  private parsePregunta(data: object): Pregunta {
-    return {id: data['id'], titulo: data['titulo'], requerido: data['requerido'], descripcion: data['descripcion'], detalle: data['detalle'], tipo: data['tipo_data']};
-  }
-
-  private generateSchemaForm(plantilla: Plantilla): Plantilla {
-    var schema = {};
-    var form = [];
-    // Definicion de tipos de datos para schema-form
-    var datatype = {'texto':{'schema':'string', 'form':'text'},
-  'rango':{'schema':'number', 'form':'number'},
-  'valor_exacto':{'schema':'number', 'form':'number'},
-  'seleccion_unica':{'schema':'string', 'form':'radios'},
-  'seleccion_multiple':{'schema':'string', 'form':'checkboxes'}};
-
-    schema['type'] = "object"; schema['title'] = plantilla.titulo; schema['required'] = [];
-    form.push({
-      "type": "help",
-      "helpvalue": "<div class=\"alert alert-info\">"+plantilla.descripcion+"</div>"
-    });
-
-    var properties = {};    
-    plantilla.secciones.forEach((seccion) => {
-      var sec = {};
-      var items = [];
-      sec['type'] = "section";
-      sec['htmlClass'] = "seccion-container";
-      items.push({
-        "type": "help",
-        "htmlClass": "col-md-12",
-        "helpvalue": "<h5 class='section-title'>"+seccion.titulo+"</h5><hr>"
-      });
-      
-      seccion.preguntas_seccion.forEach((pregunta) => {
-        var flag = pregunta.tipo['nombre'].search('seleccion')!=-1;
-        var formpregunta = {
-          "key": "p"+pregunta.id+"-"+seccion.id,
-          "type": datatype[pregunta.tipo['nombre']]['form'],
-          "titleMap": flag?pregunta.detalle:[],
-          "feedback": true,
-          "labelHtmlClass": "col-md-12",
-          "fieldHtmlClass": flag?"":"col-md-6"
-        };
-        if (flag){
-          formpregunta['title'] = pregunta.titulo;
-          /*
-          properties["p"+pregunta.id+"-"+seccion.id] = {            
-            "description": pregunta.descripcion        
-          };*/
-        } else{
-          properties["p"+pregunta.id+"-"+seccion.id] = {
-            "title": pregunta.titulo,
-            "description": pregunta.descripcion,
-            "type": datatype[pregunta.tipo['nombre']]['schema']          
-          };
-        }
-        
-        if (pregunta.requerido) {
-          schema['required'].push("p"+pregunta.id+"-"+seccion.id);
-        }
-        items.push(formpregunta);
-      });
-      
-      sec['items'] = items;
-      form.push(sec);
-      
-    });
-    schema['properties'] = properties;
-    plantilla['schema'] = schema; plantilla['form'] = form;
-    
-    return plantilla;
-  }
-
-
-
-
+  
 }
 
