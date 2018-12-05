8d8f04a59bf4f1681e18933b48c33ad3d8a2c870
Xavier Idrovo <xidrovo@hotmail.com>
2018-12-04 22:08:32 -0500

    deployment settings

--- client/inspi/src/app/api.service.ts
+++ client/inspi/src/app/api.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-
+import { environment } from '../environments/environment';
 import { throwError, of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  API_URL = 'http://localhost:8000';
+  API_URL = environment.baseUrl;
   constructor(private httpClient: HttpClient) {}
 
   getCampanias() {
