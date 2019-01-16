be969eb8aa802ce9b7ddac929524f02e80d32a0a
Xavier Idrovo <xidrovo@hotmail.com>
2019-01-16 13:01:53 -0500

    fixed (again) crear sección con múltilpes opciones
    >Also agregado el settings.txt para que todos tengan la copia del settings.py

--- client/inspi/src/app/agregar-seccion/agregar-seccion.component.ts
+++ client/inspi/src/app/agregar-seccion/agregar-seccion.component.ts
@@ -1,162 +1,170 @@
 import {
-  Component,
-  OnInit,
-  NgModule,
-  ComponentFactoryResolver,
-  Input,
-  Output,
-  EventEmitter
+    Component,
+    OnInit,
+    NgModule,
+    ComponentFactoryResolver,
+    Input,
+    Output,
+    EventEmitter
 } from '@angular/core';
 import { ApiService } from '../api.service';
 
 declare var $: any;
 @NgModule({
-  exports: [AgregarSeccionComponent],
-  entryComponents: [AgregarSeccionComponent]
+    exports: [AgregarSeccionComponent],
+    entryComponents: [AgregarSeccionComponent]
 })
 @Component({
-  selector: 'app-agregar-seccion',
-  templateUrl: './agregar-seccion.component.html',
-  styleUrls: ['./agregar-seccion.component.css']
+    selector: 'app-agregar-seccion',
+    templateUrl: './agregar-seccion.component.html',
+    styleUrls: ['./agregar-seccion.component.css']
 })
 export class AgregarSeccionComponent implements OnInit {
-  private title: String = '<Título aquí>';
-  private index: Number = 0;
-  private arrayPreguntas: any[] = [];
+    private title: String = '<Título aquí>';
+    private index: Number = 0;
+    private arrayPreguntas: any[] = [];
 
-  private titulo = '';
-  private descripcion = '';
-  private tipo_data = {id:-1, nombre: ''};
-  private requerido = false;
-  private detalle = '';
-  private tipoDatos: any;
-  private editIndex = 0;
-  private onEdit: Boolean = false;
-  //private dato = '';
+    private titulo = '';
+    private descripcion = '';
+    private tipo_data = { id: '-1', nombre: '' };
+    private requerido = false;
+    private detalle = '';
+    private tipoDatos: any;
+    private editIndex = 0;
+    private onEdit: Boolean = false;
+    //private dato = '';
 
-  private filled: Boolean = false;
+    private filled: Boolean = false;
 
-  @Output()
-  deleteClick: EventEmitter<String> = new EventEmitter<String>();
+    @Output()
+    deleteClick: EventEmitter<String> = new EventEmitter<String>();
 
-  constructor(private apiService: ApiService) {}
-  ngOnInit() {
-    this.tipoDatos = this.apiService
-      .getTipoDeDatos()
-      .subscribe((data: Array<object>) => {
-        this.tipoDatos = data;
-      });
-  }
-  setTitle(title) {
-    this.title = title;
-  }
-
-  setIndex(index) {
-    this.index = index;
-  }
-  setPreguntas(preguntas) {
-    this.arrayPreguntas = preguntas;
-  }
-  eliminar(event, index) {
-    this.deleteClick.emit(index);
-  }
-
-  addInfo(titulo, pregunta) {
-    console.log('agregando info');
-    const info = {
-      titulo: this.titulo,
-      tipo_data: this.tipo_data,
-      requerido: this.requerido,
-      detalle: this.splitDetalle(this.detalle),
-      descripcion: this.descripcion//,
-      //dato: this.dato
-    };
-    if (this.onEdit) {
-      this.arrayPreguntas[this.editIndex] = info;
-      this.onEdit = false;
-    } else {
-      this.arrayPreguntas.push(info);
+    constructor(private apiService: ApiService) {}
+    ngOnInit() {
+        this.tipoDatos = this.apiService
+            .getTipoDeDatos()
+            .subscribe((data: Array<object>) => {
+                this.tipoDatos = data;
+            });
     }
-    $('#preguntasModal' + this.index).modal('hide');
-    this.cleanModal();
-  }
-  splitDetalle(detalle) {
-    if (this.tipo_data.id != 1 && this.tipo_data.id != 2) {
-      return detalle;
+    setTitle(title) {
+        this.title = title;
     }
-    let detail = Array();
-    if (detalle !== null && detalle !== undefined) {
-      detalle = detalle.split('\n');
-      detalle.forEach((element, i) => {
-        detail.push({ value: i, name: element });
-      });
-      return detail;
+
+    setIndex(index) {
+        this.index = index;
     }
-    return '';
-  }
-  getInfo() {
-    return { titulo: this.title, preguntas: this.arrayPreguntas };
-  }
-  isArrayEmpty() {
-    return this.arrayPreguntas.length === 0;
-  }
-  onSelectChange() {
-    $('#preguntasModal' + this.index).modal('show');
-    this.tipo_data.nombre = $('#sel' + this.index + ' option:selected').text();
-  }
-
-  borrarPregunta(index) {
-    this.arrayPreguntas.splice(index, 1);
-  }
-
-  editarPregunta(index) {
-    this.editIndex = index;
-    this.onEdit = true;
-    var info = this.arrayPreguntas[index];
-    this.titulo = info['titulo'];
-    this.tipo_data = info['tipo_data'];
-    this.requerido = info['requerido'];
-    this.detalle = this.parseDetalle(info['detalle']);
-    this.descripcion = info['descripcion'];
-    $('#preguntasModal' + this.index).modal('show');
-  }
-
-  parseDetalle(detalle: any): string{
-    console.log("Detalle: ",detalle)
-    var ndetalle = '';    
-    if (this.tipo_data.id == 1 || this.tipo_data.id == 2){      
-      var size = detalle.length;
-      detalle.forEach((element,i) => {
-        ndetalle = ndetalle.concat(element['name'].concat(i<size-1?'\n':''));
-        console.log("Ndetale name: ",ndetalle);
-      });
-    } else {
-      ndetalle = ''+detalle;
+    setPreguntas(preguntas) {
+        this.arrayPreguntas = preguntas;
     }
-    console.log("Detalle resul: ",ndetalle)
-    return ndetalle;
-  }
-
-  cancelAction() {
-    this.onEdit = false;
-    this.cleanModal();
-  }
-
-  cleanModal() {
-    this.titulo = '';
-    this.tipo_data = {id:-1, nombre: ''};
-    this.requerido = false;
-    this.detalle = '';
-    this.descripcion = '';
-  }
-
-  validate() {
-    if (this.tipo_data.id == 2) {
-      this.filled = !!this.titulo && !!this.descripcion && !!this.detalle;
-    } else if (this.tipo_data.id == 1) {
-      this.filled = !!this.titulo && !!this.descripcion && !!this.detalle;
-    } else {
-      this.filled = !!this.titulo && !!this.descripcion;
+    eliminar(event, index) {
+        this.deleteClick.emit(index);
     }
-  }
+
+    addInfo(titulo, pregunta) {
+        const info = {
+            titulo: this.titulo,
+            tipo_data: this.tipo_data,
+            requerido: this.requerido,
+            detalle: this.splitDetalle(this.detalle),
+            descripcion: this.descripcion
+            //dato: this.dato
+        };
+        console.log('===detalle', info.detalle);
+        if (this.onEdit) {
+            this.arrayPreguntas[this.editIndex] = info;
+            this.onEdit = false;
+        } else {
+            this.arrayPreguntas.push(info);
+        }
+        $('#preguntasModal' + this.index).modal('hide');
+        this.cleanModal();
+    }
+    splitDetalle(detalle) {
+        if (
+            this.tipo_data.id != 'seleccion_unica' &&
+            this.tipo_data.id != 'seleccion_multiple'
+        ) {
+            return detalle;
+        }
+        let detail = Array();
+        if (detalle !== null && detalle !== undefined) {
+            detalle = detalle.split('\n');
+            detalle.forEach((element, i) => {
+                console.log('===detalle, splited', element, i);
+                detail.push({ value: i, name: element });
+            });
+            return detail;
+        }
+        return '';
+    }
+    getInfo() {
+        return { titulo: this.title, preguntas: this.arrayPreguntas };
+    }
+    isArrayEmpty() {
+        return this.arrayPreguntas.length === 0;
+    }
+    onSelectChange() {
+        $('#preguntasModal' + this.index).modal('show');
+        this.tipo_data.nombre = $(
+            '#sel' + this.index + ' option:selected'
+        ).text();
+    }
+
+    borrarPregunta(index) {
+        this.arrayPreguntas.splice(index, 1);
+    }
+
+    editarPregunta(index) {
+        this.editIndex = index;
+        this.onEdit = true;
+        var info = this.arrayPreguntas[index];
+        this.titulo = info['titulo'];
+        this.tipo_data = info['tipo_data'];
+        this.requerido = info['requerido'];
+        this.detalle = this.parseDetalle(info['detalle']);
+        this.descripcion = info['descripcion'];
+        $('#preguntasModal' + this.index).modal('show');
+    }
+
+    parseDetalle(detalle: any): string {
+        console.log('Detalle: ', detalle);
+        var ndetalle = '';
+        if (this.tipo_data.id == 1 || this.tipo_data.id == 2) {
+            var size = detalle.length;
+            detalle.forEach((element, i) => {
+                ndetalle = ndetalle.concat(
+                    element['name'].concat(i < size - 1 ? '\n' : '')
+                );
+                console.log('Ndetale name: ', ndetalle);
+            });
+        } else {
+            ndetalle = '' + detalle;
+        }
+        console.log('Detalle resul: ', ndetalle);
+        return ndetalle;
+    }
+
+    cancelAction() {
+        this.onEdit = false;
+        this.cleanModal();
+    }
+
+    cleanModal() {
+        this.titulo = '';
+        this.tipo_data = { id: -1, nombre: '' };
+        this.requerido = false;
+        this.detalle = '';
+        this.descripcion = '';
+    }
+
+    validate() {
+        if (this.tipo_data.id == 2) {
+            this.filled = !!this.titulo && !!this.descripcion && !!this.detalle;
+        } else if (this.tipo_data.id == 1) {
+            this.filled = !!this.titulo && !!this.descripcion && !!this.detalle;
+        } else {
+            this.filled = !!this.titulo && !!this.descripcion;
+        }
+    }
 }
--- client/inspi/src/app/plantillas/plantilla-editor/plantilla-editor.component.ts
+++ client/inspi/src/app/plantillas/plantilla-editor/plantilla-editor.component.ts
@@ -1,10 +1,10 @@
 import {
-  Component,
-  ComponentFactoryResolver,
-  ViewContainerRef,
-  ViewChild,
-  NgModule,
-  Input
+    Component,
+    ComponentFactoryResolver,
+    ViewContainerRef,
+    ViewChild,
+    NgModule,
+    Input
 } from '@angular/core';
 import { AgregarSeccionComponent } from '../../agregar-seccion/agregar-seccion.component';
 import { ApiService } from '../../api.service';
@@ -16,148 +16,144 @@ import { Globals } from '../../globals';
 declare var $: any;
 
 @Component({
-  selector: 'app-plantilla-editor',
-  templateUrl: './plantilla-editor.component.html',
-  styleUrls: ['./plantilla-editor.component.css'],
-  entryComponents: [AgregarSeccionComponent]
+    selector: 'app-plantilla-editor',
+    templateUrl: './plantilla-editor.component.html',
+    styleUrls: ['./plantilla-editor.component.css'],
+    entryComponents: [AgregarSeccionComponent]
 })
 export class PlantillaEditorComponent {
-  private title: string;
-  plantilla: Plantilla;
-  private payload: any = [];
-  private titulo: string;
-  private descripcion: string;
-  private canPost = false;
-  refsArray: any[] = [];
-  editMode = false;
-  @ViewChild('seccionContainer', { read: ViewContainerRef })
-  container;
-  constructor(
-    private resolver: ComponentFactoryResolver,
-    private apiService: ApiService,
-    private route: ActivatedRoute,
-    private _router: Router,
-    private globals: Globals
-  ) {}
+    private title: string;
+    plantilla: Plantilla;
+    private payload: any = [];
+    private titulo: string;
+    private descripcion: string;
+    private canPost = false;
+    refsArray: any[] = [];
+    editMode = false;
+    @ViewChild('seccionContainer', { read: ViewContainerRef })
+    container;
+    constructor(
+        private resolver: ComponentFactoryResolver,
+        private apiService: ApiService,
+        private route: ActivatedRoute,
+        private _router: Router,
+        private globals: Globals
+    ) {}
 
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      if (params.get('id') == '-1') {
-        this.editMode = true;
-        this.plantilla = this.globals.currentTemplate.plantilla;
-        this.titulo = this.plantilla.titulo;
-        this.descripcion = this.plantilla.descripcion;
-        this.plantilla.secciones.map(seccion => {
-          this.getSeccion(seccion);
+    ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            if (params.get('id') == '-1') {
+                this.editMode = true;
+                this.plantilla = this.globals.currentTemplate.plantilla;
+                this.titulo = this.plantilla.titulo;
+                this.descripcion = this.plantilla.descripcion;
+                this.plantilla.secciones.map(seccion => {
+                    this.getSeccion(seccion);
+                });
+            }
         });
-        console.log(' :P ');
-        console.log(this.plantilla);
-      }
-      console.log(params.get('id'));
-    });
-  }
-  parsePlantilla(data: any): Plantilla {
-    if (data['error'] == 0) {
-      this.titulo = data['titulo'];
-      this.descripcion = data['descripcion'];
-      console.log('Plantilla obtenida: ', this.titulo);
-      return {
-        id: data['id'],
-        titulo: data['titulo'],
-        descripcion: data['descripcion'],
-        secciones: data['secciones']
-      };
-    } else {
-      return { id: 0, titulo: '', descripcion: '', secciones: [] };
     }
-  }
-
-  getSeccion(seccion) {
-    const factory = this.resolver.resolveComponentFactory(
-      AgregarSeccionComponent
-    );
-    const componentRef = this.container.createComponent(factory);
-
-    componentRef._component.setIndex(this.refsArray.length);
-    componentRef._component.setTitle(seccion.titulo);
-    componentRef._component.setPreguntas(seccion.preguntas);
-    // De esta forma agregamos qué hacer cuando el evento 'deleteClick' sea disparado
-    componentRef.instance.deleteClick.subscribe(() => {
-      this.eliminarAlgo(componentRef._component.index);
-    });
-    this.refsArray.push(componentRef);
-    this.validate();
-    this.title = '';
-  }
-
-  // Agrega una nueva sección, llamando al componente AgregarSeccionComponent
-  addSeccion() {
-    // don't do anything if is empty
-    if (!!!this.title) {
-      return;
+    parsePlantilla(data: any): Plantilla {
+        if (data['error'] == 0) {
+            this.titulo = data['titulo'];
+            this.descripcion = data['descripcion'];
+            console.log('Plantilla obtenida: ', this.titulo);
+            return {
+                id: data['id'],
+                titulo: data['titulo'],
+                descripcion: data['descripcion'],
+                secciones: data['secciones']
+            };
+        } else {
+            return { id: 0, titulo: '', descripcion: '', secciones: [] };
+        }
     }
-    // Creo una clase de tipo AgregarSeccionComponent
-    const factory = this.resolver.resolveComponentFactory(
-      AgregarSeccionComponent
-    );
-    const componentRef = this.container.createComponent(factory);
 
-    componentRef._component.setIndex(this.refsArray.length);
-    componentRef._component.setTitle(this.title);
-    // De esta forma agregamos qué hacer cuando el evento 'deleteClick' sea disparado
-    componentRef.instance.deleteClick.subscribe(() => {
-      this.eliminarAlgo(componentRef._component.index);
-    });
-    this.refsArray.push(componentRef);
-    this.validate();
-    this.title = '';
-    $('#seccionModal').modal('hide');
-  }
+    getSeccion(seccion) {
+        const factory = this.resolver.resolveComponentFactory(
+            AgregarSeccionComponent
+        );
+        const componentRef = this.container.createComponent(factory);
 
-  // Esta función elimina la última referencia de sección agregada.
-  deleteLastSeccion() {
-    const componentRef = this.refsArray.pop();
-    componentRef.destroy();
-    this.validate();
-  }
-  // Elimina el componente del DOM y del array de Ref dado un índice.
-  eliminarAlgo(index) {
-    const componentRef = this.refsArray[index];
-    componentRef.destroy();
-    this.refsArray.splice(index, 1);
-    this.reCalculateIndex();
-    this.validate();
-  }
-  // resetea el índice de cada elemento
-  reCalculateIndex() {
-    let cont = 0;
-    this.refsArray.forEach(function(value) {
-      value._component.setIndex(cont);
-      cont++;
-    });
-  }
-  // realizar un post a la api para almacenar la plantilla
-  async guardarPlantilla() {
-    this.refsArray.forEach(x => {
-      this.payload.push(x._component.getInfo());
-    });
-    this.plantilla = {
-      id: 0,
-      titulo: this.titulo,
-      descripcion: this.descripcion,
-      secciones: this.payload
+        componentRef._component.setIndex(this.refsArray.length);
+        componentRef._component.setTitle(seccion.titulo);
+        componentRef._component.setPreguntas(seccion.preguntas);
+        // De esta forma agregamos qué hacer cuando el evento 'deleteClick' sea disparado
+        componentRef.instance.deleteClick.subscribe(() => {
+            this.eliminarAlgo(componentRef._component.index);
+        });
+        this.refsArray.push(componentRef);
+        this.validate();
+        this.title = '';
+    }
+
+    // Agrega una nueva sección, llamando al componente AgregarSeccionComponent
+    addSeccion() {
+        // don't do anything if is empty
+        if (!!!this.title) {
+            return;
+        }
+        // Creo una clase de tipo AgregarSeccionComponent
+        const factory = this.resolver.resolveComponentFactory(
+            AgregarSeccionComponent
+        );
+        const componentRef = this.container.createComponent(factory);
+
+        componentRef._component.setIndex(this.refsArray.length);
+        componentRef._component.setTitle(this.title);
+        // De esta forma agregamos qué hacer cuando el evento 'deleteClick' sea disparado
+        componentRef.instance.deleteClick.subscribe(() => {
+            this.eliminarAlgo(componentRef._component.index);
+        });
+        this.refsArray.push(componentRef);
+        this.validate();
+        this.title = '';
+        $('#seccionModal').modal('hide');
+    }
+
+    // Esta función elimina la última referencia de sección agregada.
+    deleteLastSeccion() {
+        const componentRef = this.refsArray.pop();
+        componentRef.destroy();
+        this.validate();
+    }
+    // Elimina el componente del DOM y del array de Ref dado un índice.
+    eliminarAlgo(index) {
+        const componentRef = this.refsArray[index];
+        componentRef.destroy();
+        this.refsArray.splice(index, 1);
+        this.reCalculateIndex();
+        this.validate();
+    }
+    // resetea el índice de cada elemento
+    reCalculateIndex() {
+        let cont = 0;
+        this.refsArray.forEach(function(value) {
+            value._component.setIndex(cont);
+            cont++;
+        });
+    }
+    // realizar un post a la api para almacenar la plantilla
+    async guardarPlantilla() {
+        this.refsArray.forEach(x => {
+            this.payload.push(x._component.getInfo());
+        });
+        this.plantilla = {
+            id: 0,
+            titulo: this.titulo,
+            descripcion: this.descripcion,
+            secciones: this.payload
+        };
+        if (this.editMode) {
+            this.plantilla.id = this.globals.currentTemplate.plantilla.id;
+            await this.apiService.setPlantilla(this.plantilla);
+        } else {
+            await this.apiService.addPlantilla(this.plantilla);
+        }
+        this._router.navigate(['/plantillas']);
+    }
+    validate = () => {
+        const someSection = this.refsArray.length > 0;
+        this.canPost = !!this.titulo && !!this.descripcion && someSection;
     };
-    if (this.editMode) {
-      this.plantilla.id = this.globals.currentTemplate.plantilla.id;
-      console.log(this.plantilla);
-      await this.apiService.setPlantilla(this.plantilla);
-    } else {
-      await this.apiService.addPlantilla(this.plantilla);
-    }
-    this._router.navigate(['/plantillas']);
-  }
-  validate = () => {
-    const someSection = this.refsArray.length > 0;
-    this.canPost = !!this.titulo && !!this.descripcion && someSection;
-  };
 }
